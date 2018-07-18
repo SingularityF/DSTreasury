2019408321b25292a982feb2b19c62397fdcb0d3
SingularityF <mailtosingularityf@gmail.com>
2018-07-18 15:23:10 -0500

    Improved Tk input process

--- libmod/convenience.py
+++ libmod/convenience.py
@@ -3,26 +3,46 @@ import tkinter.filedialog
 import pandas as pd
 
 class Input:
+    path2file=""
     path2files=[]
-    def path_gui(self):
+    def path_gui(self,message=""):
         """Run Tk window to choose files        
         """
         tk_root=tk.Tk()
         tk_root.title("File select window")
+        tk_root.configure(background='white')
         def sel_files_callback(event):
             self.path2files=list(tk.filedialog.askopenfilenames())
+            try:
+                self.path2file=self.path2files[0]
+            except:
+                pass
             tk_root.destroy()
         def sel_dir_callback(event):
             self.path2files=glob.glob(tk.filedialog.askdirectory()+"/*")
+            try:
+                self.path2file=self.path2files[0]
+            except:
+                pass
             tk_root.destroy()
+        msg_txt=tk.Text(tk_root)
+        msg_txt.insert(1.0, message)
+        msg_txt.config(state=tk.DISABLED)
+        msg_txt.config(wrap=tk.WORD)
         sel_dir_btn=tk.Button(tk_root,text="Select directory")
         sel_files_btn=tk.Button(tk_root,text="Select files")
         sel_files_btn.bind("<ButtonPress-1>",sel_files_callback)
         sel_dir_btn.bind("<ButtonPress-1>",sel_dir_callback)
-        sel_files_btn.place(x=50,y=10,width=100)
-        sel_dir_btn.place(x=50,y=40,width=100)
+        sel_files_btn.place(x=50,y=100,width=100)
+        sel_dir_btn.place(x=50,y=130,width=100)
+        msg_txt.place(x=-1,y=-1,width=200,height=80)
         tk_root.mainloop()
         return self.path2files
+    
+    def load_single(self,enc="utf-8"):
+        """Load the first selected file and return dataframe
+        """
+        return self.universal_load(self.path2file,enc=enc)
     
     def universal_load(self,path,enc="utf-8"):
         """Load dataframe by file extension
